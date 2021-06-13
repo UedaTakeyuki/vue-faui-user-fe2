5b429b5b1a69233032038f25cf62c84d216cbed8
Takeyuki UEDA <ueda@latelierdueda.com>
2021-06-13 22:25:06 +0900

    fix

--- src/build-entry.js
+++ src/build-entry.js
@@ -1,9 +1,9 @@
-import AccountFront from '@/views/account/AccountFront.vue';
-import PurchaseFront from '@/views/purchase/PurchaseFront.vue';
-import AccountRoutes from '@/views/account/account.route.js'
-import PurchaseRoutes from '@/views/purchase/purchase.route.js'
-import Login from '@/components/Login2.vue'
-import EmailVerificationRequest from '@/components/EmailVerificationRequest2.vue'
+import AccountFront from './views/account/AccountFront.vue';
+import PurchaseFront from './views/purchase/PurchaseFront.vue';
+import AccountRoutes from './views/account/account.route.js'
+import PurchaseRoutes from './views/purchase/purchase.route.js'
+import Login from './components/Login.vue'
+import EmailVerificationRequest from './components/EmailVerificationRequest.vue'
 
 
 // Named export
